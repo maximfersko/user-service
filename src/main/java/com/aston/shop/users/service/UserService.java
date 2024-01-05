b13c8d801c8c88084c63fd42f845bd4ec678afac
maximfersko <79246330+maximfersko@users.noreply.github.com>
2024-01-06 01:32:49 +0700

    refactoring

--- src/main/java/com/aston/shop/users/service/UserService.java
+++ src/main/java/com/aston/shop/users/service/UserService.java
@@ -51,8 +51,8 @@ public class UserService {
 	}
 
 	@Transactional
-	public void save(User user) {
-		userRepository.save(user);
+	public UserDto save(User user) {
+		return UserMapper.INSTANCE.toDto(userRepository.save(user));
 	}
 
 	@Transactional
