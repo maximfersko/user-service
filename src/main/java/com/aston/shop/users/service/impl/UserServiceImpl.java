b344737cd25cd76193bd8461e39b8d66ad7d8d87
maximfersko <79246330+maximfersko@users.noreply.github.com>
2024-01-19 07:09:34 +0700

    done auth & register

---
+++ src/main/java/com/aston/shop/users/service/impl/UserServiceImpl.java
@@ -0,0 +1,74 @@
+package com.aston.shop.users.service.impl;
+
+
+import com.aston.shop.users.entity.User;
+import com.aston.shop.users.repository.UserRepository;
+import com.aston.shop.users.security.JwtUserDetails;
+import com.aston.shop.users.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+@Slf4j
+@Transactional(readOnly = true)
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+	private final UserRepository userRepository;
+
+	@Override
+	public boolean existsByUsername(String username) {
+		return userRepository.existsByUsername(username);
+	}
+
+	@Override
+	public boolean existsByEmail(String email) {
+		return userRepository.existsByEmail(email);
+	}
+
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+
+	public Optional<User> findById(Long id) {
+		return userRepository.findById(id);
+	}
+
+	@Override
+	public UserDetails findByUsername(String username) {
+		Optional<User> userOptional = userRepository.findOneByUsername(username);
+		User user = userOptional.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+		return new JwtUserDetails(user);
+	}
+
+	@Override
+	public UserDetailsService userDetailsService() {
+		return this::findByUsername;
+	}
+
+
+	@Transactional
+	public void update(User user) {
+		userRepository.saveAndFlush(user);
+	}
+
+	@Transactional
+	public void save(User user) {
+		userRepository.save(user);
+	}
+
+	@Transactional
+	public void deleteById(Long id) {
+		userRepository.deleteById(id);
+	}
+
+}
